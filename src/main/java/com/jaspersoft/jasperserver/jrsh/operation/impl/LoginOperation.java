a3275ec244248dbefd27d5cf6e882895ebfeb60c
Krasnyanskiy <krasnyanskiy.alex@gmail.com>
2015-08-01 15:51:58 +0300

    Added Login operation.

---
+++ src/main/java/com/jaspersoft/jasperserver/jrsh/operation/impl/LoginOperation.java
@@ -0,0 +1,15 @@
+package com.jaspersoft.jasperserver.jrsh.operation.impl;
+
+import com.jaspersoft.jasperserver.jaxrs.client.core.Session;
+import com.jaspersoft.jasperserver.jrsh.operation.Operation;
+import com.jaspersoft.jasperserver.jrsh.operation.result.OperationResult;
+
+/**
+ * @author Alexander Krasnyanskiy
+ */
+public class LoginOperation implements Operation {
+    @Override
+    public OperationResult execute(Session session) {
+        return null;
+    }
+}
